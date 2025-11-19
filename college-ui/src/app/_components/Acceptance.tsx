fa6fece9f1073cf1e4d05d22e7365dcf8bf17e82
Velraaj <velraaj30@gmail.com>
2025-11-19 19:36:07 +0530

    - Added a new component named acceptance

---
+++ college-ui/src/app/_components/Acceptance.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Acceptance = () => {
+  return (
+    <>
+        <div className='acceptance'>
+            <div className='acceptanceWrap'>
+                <div className='container'>
+                    <div className='acceptancePageArea p-10' >
+                        <div className='acceptancePageHeader'>
+                            <h5 className='text-center mb-7'>Where We Go From Here</h5>
+                        </div>
+                        <div className='acceptancePageContent flex flex-wrap flex-col justify-center items-center'>
+                            <h2 className='text-center mb-7 text-4xl'>College Acceptances</h2>
+                            <p className='text-center max-w-[770] mb-7 text-xl text-gray-500'>
+                                Stanford? Yale? WashU? Navy? Yep, our students get into the most selective schools in the country. But our focus is on helping them develop curiosity, skills, and confidence and then finding the college that's the best fit for them. Every Griffin student gets personalized, proactive guidance to navigate the admissions process to produce college options that work for them. A sampling of colleges who have accepted Griffin grads are below.
+                            </p>
+                        </div>
+                        <div className='logoCarousal'>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Acceptance
