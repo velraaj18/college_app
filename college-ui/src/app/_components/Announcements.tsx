6f1834afbbe8740a1c5a278de16c7861aa0da57a
Velraaj <velraaj30@gmail.com>
2025-11-20 20:46:16 +0530

    - Completed the Announcements comp - Added a new comp Highlights - Added new images for the highlights comp

--- college-ui/src/app/_components/Announcements.tsx
+++ college-ui/src/app/_components/Announcements.tsx
@@ -9,15 +9,18 @@ const Announcements = () => {
           <div className="container">
             <div className="announcementsPageArea p-10">
               <div className="announcementsPageHeader">
-                <h5 className="text-center mb-7">What's Happening</h5>
+                <h5 className="text-center text-2xl mb-10">What's Happening</h5>
               </div>
               <div className="announcementsPageContent flex flex-wrap flex-col justify-center items-center">
-                <h2 className="text-center mb-7 text-4xl">
+                <h2 className="text-center mb-10 text-4xl">
                   Events & Announcements
                 </h2>                         
               </div>
-              <div className='max-w-[1080px] mx-auto'>
+              <div className='max-w-[1080px] mx-auto mb-14'>
                 <EventCards/>
+              </div>
+              <div className='max-w-[1280px] flex mt-5 mb-10'>
+                <img src="images/icon-full-calendar2.svg" alt="" className='ml-auto'/>
               </div>
             </div>
           </div>
---
+++ college-ui/src/app/_components/Highlights.tsx
@@ -0,0 +1,13 @@
+import { Carousel } from "primereact/carousel"
+
+const Highlights = () => {
+    return(
+        <>
+            <div>
+                <Carousel/>
+            </div>
+        </>
+    )
+}
+
+export default Highlights
