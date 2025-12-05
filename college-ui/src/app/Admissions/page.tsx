bce2bc178b2e62eff0c2818fd9d8e087c24611fd
Velraaj <velraaj30@gmail.com>
2025-12-06 04:10:09 +0530

    - Started Admissions page

--- college-ui/src/app/Admissions/page.tsx
+++ college-ui/src/app/Admissions/page.tsx
@@ -1,20 +1,38 @@
-import React from 'react'
-import Navbar from '../_components/Navbar'
-import styles from './admissions.module.css'
+import React from "react";
+import Navbar from "../_components/Navbar";
+import styles from "./admissions.module.css";
 
 const AdmissionPage = () => {
   return (
     <>
-        <Navbar/>
-        <div className='carousalImage'>
-            <div className={styles.admissionCarousal}>
-                <div className=''>
-                    
+      <Navbar />
+      <main className="pt-32">
+        <div className="carousalImage">
+          <div className={styles.admissionCarousal}>
+            <div className={`${styles.admissionContent} relative z-10 `}>
+              <div className="container max-w-[1080px]">
+                <div className={styles.admissionInner}>
+                  <h1 className={styles.admissionHeader}>Admissions</h1>
+                  <img src="/images/banner-squggle.svg" alt="banner-squggle" />
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </>
-  )
-}
+        <div className="container max-w-[1080px] mt-7">
+          <h1 className={styles.contentLarge}>We welcome applications from bright students who are seeking a creative and personalized school environment.</h1>
+        </div>
+        <div className={`${styles.content1} mt-7`}>
+          <div className={styles.content1Left}>
 
-export default AdmissionPage
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AdmissionPage;
