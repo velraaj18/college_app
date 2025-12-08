9c4e50f8f02d64b19425f0100a2cf67af266169d
Velraaj <velraaj30@gmail.com>
2025-12-08 20:57:48 +0530

    - Updated the admission page

--- college-ui/src/app/Admissions/page.tsx
+++ college-ui/src/app/Admissions/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../_components/Navbar";
 import styles from "./admissions.module.css";
+import Spacer from "../_components/Spacer";
+import Footer from "../_components/Footer";
 
 const AdmissionPage = () => {
   return (
@@ -19,18 +21,78 @@ const AdmissionPage = () => {
             </div>
           </div>
         </div>
-        <div className="container max-w-[1080px] mt-7">
-          <h1 className={styles.contentLarge}>We welcome applications from bright students who are seeking a creative and personalized school environment.</h1>
-        </div>
-        <div className={`${styles.content1} mt-7`}>
-          <div className={styles.content1Left}>
 
-          </div>
-          <div>
-            
+        <div className="container max-w-[1080px] mt-7">
+          <h1 className={styles.contentLarge}>
+            We welcome applications from bright students who are seeking a
+            creative and personalized school environment.
+          </h1>
+        </div>
+
+        <div className={`${styles.content1} mt-7`}>
+          <div className={styles.content1Left}></div>
+          <div className={styles.content1RightContainer}>
+            <div className={styles.content1Right}>
+              <h2 className=" text-[32px] font-bold mb-5">Come Check us Out!</h2>
+              <p className="para">
+                We are thrilled that you are considering Griffin School for your
+                high school experience and look forward to connecting! The best
+                way to learn more about us is through one of our fall Open
+                Houses or through a family tour. We're hosting Open Houses on
+                September 25 and October 28, 2025. On both nights, prospective
+                athletes are invited to come at 4:45pm to our Trapezoid location
+                down the street (5206 Eilers Ave) to learn about our athletics
+                program. Tours on our main campus (5001 Evans Ave) will start at
+                5:15pm. If you are unable to make it to these events, don't
+                hesitate to reach out to admissions@griffinschool.org to set up
+                a tour. We are flexible and would love to find a time to walk
+                you around campus to give a feel for our learning environment
+                and vibrant community! Tours can happen between 9am and 4pm each
+                weekday during the school year.
+              </p>
+            </div>
+            <div></div>
           </div>
         </div>
+        <Spacer />
+
+        <div className={`${styles.content2} mt-7`}>
+          <div className={styles.content2LeftContainer}>
+            <div className={styles.content2Left}>
+              <h2 className=" text-[32px] font-bold mb-5">Application Process</h2>
+              <p className="para">
+                We are thrilled that you are considering Griffin School for your
+                high school experience and look forward to connecting! The best
+                way to learn more about us is through one of our fall Open
+                Houses or through a family tour. We're hosting Open Houses on
+                September 25 and October 28, 2025. On both nights, prospective
+                athletes are invited to come at 4:45pm to our Trapezoid location
+                down the street (5206 Eilers Ave) to learn about our athletics
+                program. Tours on our main campus (5001 Evans Ave) will start at
+                5:15pm. If you are unable to make it to these events, don't
+                hesitate to reach out to admissions@griffinschool.org to set up
+                a tour. We are flexible and would love to find a time to walk
+                you around campus to give a feel for our learning environment
+                and vibrant community! Tours can happen between 9am and 4pm each
+                weekday during the school year.
+              </p>
+            </div>
+          </div>
+          <div className={styles.content2Right}></div>
+        </div>
+        <Spacer/>
+
+        <div className={`${styles.content3} mt-7`}>
+          <div className="container max-w-[1080px]">
+            <h2 className="text-[32px]">The application portal for 2026-27 will open in late September:</h2>
+            <br />
+            <h2 className="text-[32px]">Four Easy Steps to Apply:</h2>
+          </div>
+        </div>
+        <Spacer/>
+
       </main>
+      <Footer/>
     </>
   );
 };
---
+++ college-ui/src/app/_components/Spacer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spacer = () => {
+  return (
+    <div className='spacer'>
+
+    </div>
+  )
+}
+
+export default Spacer
